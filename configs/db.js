a20c861aa3ccd310050c8b411feabe8482242414
yban <laubana@gmail.com>
2025-01-28 23:25:31 -0800

    change env

--- configs/db.js
+++ configs/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 export const connect = async () => {
-  const client = await MongoClient.connect(process.env.MONGO_URI);
+  const client = await MongoClient.connect(process.env.DB_URI);
 
   return client;
 };
